29b6796c217c0ccdcc15f933dd1a19183f7f8760
Alumno <Alumno>
2017-04-04 12:51:03 -0500

    crear event dispatcher y plantilla display object

--- myapp/app/src/main/java/com/tecsup/sanchez/myapp/DisplayObject.java
+++ myapp/app/src/main/java/com/tecsup/sanchez/myapp/DisplayObject.java
@@ -1,23 +1,44 @@
 package com.tecsup.sanchez.myapp;
 
+import android.app.usage.UsageEvents;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.view.MotionEvent;
+import android.view.animation.Transformation;
 
 /**
  * Created by Heat on 01/11/2016.
  */
-public class DisplayObject {
+public abstract class DisplayObject extends EventDispatcher {
 
+    public int height, width;
+    public float opacity, scaleX, scaleY;
+    public int pivotX, pivotY, rotation;
+    public Transformation transformation;
+    public boolean visible;
     public int x,y;
     public DisplayObject parent;
     public Resources resources;
 
+    public DisplayObject () {}
+    public void dispatchEvent (/*Event e*/){}
+    public void dispatchEvent (String e){}
+    public void dispose (){}
+    public Point globlalToLocal (Point p) {}
+    public Point localToGlobal (Point p) {}
+    public abstract void onDraw ();
+    public void primaryOperations(){}
+    public void touchDown (Point p, int index){}
+    public void touchMove (Point p, int index){}
+    public void touchUp  (Point p, int index){}
+
+
+
+
+
+
     public DisplayObject(Resources resources){
         this.resources = resources;
-    }
-
-    public void onDraw(Canvas canvas){
-        this.update();
     }
 
     public void update(){
---
+++ myapp/app/src/main/java/com/tecsup/sanchez/myapp/EventDispatcher.java
@@ -0,0 +1,17 @@
+package com.tecsup.sanchez.myapp;
+
+/**
+ * Created by Alumno on 04/04/2017.
+ */
+
+public class EventDispatcher {
+
+    public EventDispatcher () {}
+    public void addEventListener (String type){}
+    public void dispatchEvent (/*Event e*/){}
+    public void dispatchEvent (String type){}
+    public void dispose () {}
+    public void recieveEvent (/*Event e*/) {}
+    public void removeEventListener (String type){}
+    public void removeEventListeners (){}
+}
